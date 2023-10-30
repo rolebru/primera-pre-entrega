deb9b829fa0d04f0e4aecb97d700697079cc7574
Rodrigo Valdivieso <vrodrigoaaron@gmail.com>
2023-10-29 23:29:51 -0300

    ejemplo entrega

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
-// Solicitamos un valor al usuario
-let ingresarNumero = parseInt(prompt("Ingresar Numero en Pesos"));
-// En cada repetición, calculamos el número ingresado x el número de
-for (let i = 1040; i <= 1050; i++) {
-    let resultado = ingresarNumero / i ;
-    alert(ingresarNumero +" Pesos a "+ i +" = "+ resultado +" Dolares ");
+
+function loguear() {
+
+//sistema de reserva de cancha con usuario
+
+let identificar = true // lo que me valida todo en while
+let intentos = 1
+
+do{
+    let usuario = prompt("ingresa tu usuario (solo 3 intentos)")
+    if(usuario ===null){//si es null se salta al sig paso
+        break
+    }
+
+    if(usuario === "rodrigo valdivieso" && intentos <=3){
+        alert("Bienvenido " + usuario)
+        identificar = false //para que no se vuelva a ejecutar
+    }else{
+        alert("no se reconoce usuario " + usuario)
+        intentos++
+        if(intentos > 3){
+            alert("supero los 3 intentos, intente mas tarde")
+            console.error("user is not define")
+            break
+        }
+    }
+
+
+} while(identificar)
+
+
 }
