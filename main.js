ebf06ca1350bc57254aef41f0ed65c9eaf2a90ee
Rodrigo Valdivieso <vrodrigoaaron@gmail.com>
2023-12-06 13:56:41 -0300

    modificando preentrega3

--- main.js
+++ main.js
@@ -32,39 +32,95 @@ let verdura15 = new Verdura("puerro", 24, 100)
 let lista = [verdura1,verdura2,verdura3,verdura4,verdura5,verdura6,verdura7,verdura8,verdura9,verdura10,verdura11,verdura12,verdura13,verdura14,verdura15]
 
 
-function filtrarVerduras(){ //lo usamos en el console para wue nos salte el prompt
+if(localStorage.getItem("verduras")){
+    lista = JSON.parse(localStorage.getItem("verduras"))
+}else{
+    lista = lista
+}
 
-    let palabraClave = prompt("ingresa la Verdura que buscas").toUpperCase().trim()
-    let resultado = lista.filter((x)=>x.nombre.toUpperCase().includes(palabraClave))
+
+function filtrarVerduras(){
+    const body = document.querySelector("body")
+    const input = document.getElementById("filtrarV").value 
+    const palabraClave = input.trim().toUpperCase()
+    const resultado = lista.filter(  (verdura)=> verdura.nombre.toUpperCase().includes(palabraClave))
+
+    if(resultado.length  > 0){
+
+
+        const container = document.createElement("div")
+        container.classList.add("container")
+
+        resultado.forEach( (verdura)=>{
+
+            const card = document.createElement("div")
+
+
+        const nombre = document.createElement("h2")
+        nombre.textContent = `Nombre: ${verdura.nombre}`
+        card.appendChild(nombre)
+
+        const Kcal = document.createElement("p")
+        Kcal.textContent = `Kcal: ${verdura.Kcal}`
+        card.appendChild(Kcal)
+
+        const gramos = document.createElement("p")
+        gramos.textContent = `Gramos: ${verdura.gramos}`
+        card.appendChild(gramos)
+
+        container.appendChild(card)
+
+        } )
+
+        body.appendChild(container)
 
 
 
-    if(resultado.length >0){
-        console.table(resultado)
     }else{
-        alert("no se encontro verdura " + palabraClave)
+        alert("no hay coincidencia")
     }
-
-
 }
 
 
-function agregarVerdura(){
-
-    let nombre = prompt("ingresa el nombre de la verdura")
-    let Kcal = parseFloat(prompt("ingresa las Kcal de la verdura")) 
-    let gramos = parseInt(prompt("ingresa los gramos de la verdura"))
 
 
-    if(isNaN(Kcal) || isNaN(gramos) || nombre===""){
-        alert("por favor ingresa valores validos")
-        return
-    }
+const filtrarBoton = document.getElementById("boton")
+filtrarBoton.classList.add("button")
+filtrarBoton.addEventListener("click",filtrarVerduras)
 
-    let verdura = new Verdura(nombre,Kcal,gramos)
+// function filtrarVerduras(){ //lo usamos en el console para wue nos salte el prompt
 
-    lista.push(verdura)
-    console.table(lista)
+//     let palabraClave = prompt("ingresa la Verdura que buscas").toUpperCase().trim()
+//     let resultado = lista.filter((x)=>x.nombre.toUpperCase().includes(palabraClave))
 
 
-}
+
+//     if(resultado.length >0){
+//         console.table(resultado)
+//     }else{
+//         alert("no se encontro verdura " + palabraClave)
+//     }
+
+
+// }
+
+
+// function agregarVerdura(){
+
+//     let nombre = prompt("ingresa el nombre de la verdura")
+//     let Kcal = parseFloat(prompt("ingresa las Kcal de la verdura")) 
+//     let gramos = parseInt(prompt("ingresa los gramos de la verdura"))
+
+
+//     if(isNaN(Kcal) || isNaN(gramos) || nombre===""){
+//         alert("por favor ingresa valores validos")
+//         return
+//     }
+
+//     let verdura = new Verdura(nombre,Kcal,gramos)
+
+//     lista.push(verdura) 
+//     console.table(lista)
+
+
+// }
